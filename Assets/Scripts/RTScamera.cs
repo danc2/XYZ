f1371d47702e69a093fdfd6cd16eac892e0597a6
danc2 <channyboy5@hotmail.com>
2016-10-07 00:21:37 +1100

    improved the camera

--- Assets/Scripts/RTScamera.cs
+++ Assets/Scripts/RTScamera.cs
@@ -13,6 +13,12 @@ public class RTScamera : MonoBehaviour {
 	public float minFOV = 10;
 	public float maxFOV = 30;
 
+	public float mapBorderForward = 467f;
+	public float mapBorderBack = -34f;
+	public float mapBorderLeft = -38f;
+	public float mapBorderRight = 448f;
+
+
 	// Use this for initialization
 	void Start () {
 		Camera.main.fieldOfView = distance;
@@ -28,21 +34,49 @@ public class RTScamera : MonoBehaviour {
 			           { 
 
 			transform.Translate(Vector3.left * CameraSpeed * Time.deltaTime);
+			if (transform.position.x < -mapBorderLeft)
+			{
+                Vector3 clampLeft = transform.position;
+				clampLeft.x = -mapBorderLeft;
+				transform.position = clampLeft;
+			}
+
 		} 
 
 		if (mousePosX >= Screen.width - scrollDistance || Input.GetKey(KeyCode.D)) 
 			         { 
 			transform.Translate(Vector3.right * CameraSpeed * Time.deltaTime);
+			if (transform.position.x > mapBorderRight)
+			{
+				Vector3 clampRight = transform.position;
+				clampRight.x = mapBorderRight;
+	            transform.position = clampRight;
+			}
+
 		}
 
 		if (mousePosY < scrollDistance || Input.GetKey(KeyCode.S)) 
 			           { 
 			transform.Translate(Vector3.down * CameraSpeed * Time.deltaTime);
+			if (transform.position.z < -mapBorderBack)
+			{
+				Vector3 clampBack = transform.position;
+				clampBack.z = -mapBorderBack;
+				transform.position = clampBack;
+             }
+			
 		} 
 
 		if (mousePosY >= Screen.height - scrollDistance || Input.GetKey(KeyCode.W))
 		           { 
 			transform.Translate(Vector3.up * CameraSpeed * Time.deltaTime);
+			if (transform.position.z > mapBorderForward)
+			{
+				Vector3 clampForward = transform.position;
+				clampForward.z = mapBorderForward;
+				transform.position = clampForward;
+			}
+
 		}
 
 		distance -= Input.GetAxis("Mouse ScrollWheel") * sensitivityDistance;
