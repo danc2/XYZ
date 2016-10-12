4730bc1b3ffa59ebd3775f74c40c899b24e56908
danc2 <channyboy5@hotmail.com>
2016-10-12 18:01:59 +1100

    changes to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,40 +2,85 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-    public GameObject currentSelection;
+
+	[Header("Units")]
+	public GameObject currentSelection;
     public GameObject[] turrets;
 
+	[Header("Waves Settings")]
+	[Range(1,10)]
+	public int NumberOfWave;
+
+	[Header("Health Settings")]
+	public int UnitsHealth = 100;
+	public int FPSHealth = 100;
+	public int EnemyHealth = 100;
+
+	[Header("FPS Mode")]
+	public bool FPSAvailable;
+
+	[Header("Camera Control")]
+	public bool FPSon;
+	public bool RTSon;
+	public GameObject FPS;
+	public GameObject RTS;
+
+	[Header("HUD Settings")]
+	public GUIText text;
+	float period = 4.0f;
 
     // Use this for initialization
     void Start()
     {
+		RTSon = true;
+		FPSon = false;
+
         currentSelection = null;
 
     }
 
     // Update is called once per frame
     void Update()
-    {
-        if (currentSelection != null)
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit = new RaycastHit();
+	{
+		if (currentSelection != null) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit = new RaycastHit ();
 
-            if (Physics.Raycast(ray, out hit, 100f))
-            {
-                Debug.Log("raycast hit!");
-                if (hit.transform.tag == "Ground")
-                {
-                    Debug.Log("raycast hit Ground!");
-                    if (Input.GetMouseButtonUp(0))
-                    {
-                        Instantiate(currentSelection, hit.point, Quaternion.identity);
-                        currentSelection = null;
-                    }
-                }
-            }
-        }
-}
+			if (Physics.Raycast (ray, out hit, 1000f)) {
+				Debug.Log ("raycast hit!");
+				if (hit.transform.tag == "Ground") {
+					Debug.Log ("raycast hit Ground!");
+					if (Input.GetMouseButtonUp (0)) {
+						Instantiate (currentSelection, hit.point, Quaternion.identity);
+						currentSelection = null;
+					}
+				}
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.F)) {
+			if (RTSon == true && FPSAvailable == true) {
+				FPSon = true;
+				RTSon = false;
+				FPS.SetActive (true);
+				RTS.SetActive (false);
+				Cursor.visible = false;
+			} else if (FPSon == true) {
+				FPSon = false;
+				RTSon = true;
+				FPS.SetActive (false);
+				RTS.SetActive (true);
+				Cursor.visible = true;
+			} 
+
+		}
+		if (FPSAvailable == false) {
+			period += Time.deltaTime;
+			if(Time.time > period){
+				Destroy(text);
+			}
+
+		}
+	}
     public void SelectTurret(int i)
     {
         if (i < turrets.Length)
