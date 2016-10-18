d8e1b1890139f94513a82d7b60dced1216ed0929
jamiedspencer <jamiedspencer@googlemail.com>
2016-10-18 16:08:09 +1000

    Started to change the amount of damage each type of turret does

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,45 +1,48 @@
 ﻿using UnityEngine.UI;
 using UnityEngine;
 using System.Collections;
- 
 
 
-public class EnemyHealth : MonoBehaviour {
-	public float maxHealth	= 1000;
-	public float curHealth = 100;
-	public static bool death;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float maxHealth = 1000;
+    public float curHealth = 100;
+    public static bool death;
     // daniel can you put this back in - I couldnt find the asset for the tank
     //public Slider Slider;
-   // Use this for initialization
+    // Use this for initialization
 
 
-	void Start() {
-		//Slider.value = maxHealth;
+    void Start()
+    {
+        //Slider.value = maxHealth;
 
-		}
-
-
-
-		
+    }
 
 
- // Update is called once per frame
 
 
-	 void Update () {
 
-		//Slider.value =  curHealth;
-	  	
-		if (curHealth < 0)
+
+    // Update is called once per frame
+
+
+    void Update()
+    {
+
+        //Slider.value =  curHealth;
+
+        if (curHealth < 0)
         {
-			Destroy (gameObject);
+            Destroy(gameObject);
 
-		}
+        }
 
-		   }
+    }
     // https://unity3d.com/learn/tutorials/topics/physics/detecting-collisions-oncollisionenter
-
-    void OnCollisionEnter(Collision col){
+    void OnCollisionEnter(Collision col)
+    {
         // detect the hit
         Debug.Log("Hit");
         Debug.Log(col.gameObject.tag);
@@ -48,16 +51,16 @@ public class EnemyHealth : MonoBehaviour {
         {
             //remove 10 health
             curHealth = curHealth - 10;
-            Debug.Log("Took Damage");
+            Debug.Log("Took Damage from bullet");
 
-		}
-	}
-  
+        }
+        // the object that has hit it is a missile
+        if (col.gameObject.tag == "Missile")
+        {
+            //remove 10 health
+            curHealth = curHealth - 50;
+            Debug.Log("Took Damage from missile");
 
-
-
-
-		    }
-
-
-	
+        }
+    }
+}
